5dd0a29de5569288cc837a8f79157ffae89ff576
Mohit Gupta <mohit.gupta@arcsinfotech.com>
2024-04-15 18:41:10 +0530

    response handling in interceptor

--- src/hooks/useInterceptor.ts
+++ src/hooks/useInterceptor.ts
@@ -1,19 +1,43 @@
 "use client";
-import { getAuthToken } from "@/services/frontend/storage.service";
+import {
+  getAuthToken,
+  setAuthToken,
+} from "@/services/frontend/storage.service";
+import { redirect } from "next/navigation";
 import { useEffect } from "react";
 
 const apiInterceptor = () => {
   const originalFetch = window.fetch;
 
-  window.fetch = async (input: RequestInfo | URL, init: RequestInit = {}): Promise<Response> => {
+  window.fetch = async (
+    input: RequestInfo | URL,
+    init: RequestInit = {}
+  ): Promise<Response> => {
     const token = getAuthToken();
-    const headers: any = {...init.headers};
-    if(token && token !== ''){
-      headers['Authorization'] = `Bearer ${token}`;
+    const headers: any = { ...init.headers };
+    if (token && token !== "") {
+      headers["Authorization"] = `Bearer ${token}`;
     }
     init.headers = headers;
+    return new Promise(async (resolve, reject) => {
+      try {
+        const res = await originalFetch(input as Request, init);
+        if ([400, 401, 422, 404, 419, 500, 403].includes(res.status)) {
+          const response = await res.json();
+          if (res.status === 401 || res.status == 403) {
+            setAuthToken("");
+            redirect("/login");
+          }
+          console.log(response);
+          return reject(response);
+        }
 
-    return originalFetch(input, init);
+        return resolve(res);
+      } catch (error: any) {
+        console.log(error);
+        return reject(error);
+      }
+    });
   };
 };
 
